1c0cddc6b44fcc554c97234a55c501f6ff378785
Cliff.Wu <cliff.wu@thinkpower-info.com>
2019-12-09 02:54:41 +0800

    Default

---
+++ RuntimeXaml/RuntimeXaml/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using RuntimeXaml.Models;
+
+namespace RuntimeXaml.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
